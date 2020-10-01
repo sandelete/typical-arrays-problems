b5ee996dbd5031bf10c580bc92921c6fbfd3aeea
Саша Ряхов <rkhv@MacBook-Pro-Sasa.local>
2020-10-01 22:34:24 +0300

    feat: add all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if ((Array.isArray(arr)) && arr.length > 0) {
+    let result = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < result) {
+      result = arr[i];
+    }
+  }
+
+  return result;
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if ((Array.isArray(arr)) && arr.length > 0) {
+    let result = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > result) {
+      result = arr[i];
+    }
+  }
+
+  return result;
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if ((Array.isArray(arr)) && arr.length > 0) {
+    let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return result = sum / arr.length;
+  } else {
+    return 0;
+  }
 }
